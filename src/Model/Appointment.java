405f411c5f9be8d60325648ef2246e10f0f280b1
Bryan Leanoe <bryan.leano@gmail.com>
2020-09-24 19:57:02 -0600

    add appointment constructor

--- src/Model/Appointment.java
+++ src/Model/Appointment.java
@@ -4,4 +4,27 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Appointment {
+
+    private int id;
+    private String name;
+    private String title;
+    private String type;
+    private String date;
+    private String startTime;
+    private String endTime;
+
+    public Appointment(int id, String name, String title, String type, String date,
+                       String startTime, String endTime) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.type = type;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+
+
+
 }
