4f9733e20ac2e712c4aeeab74567eb4b1970bbb0
Bryan Leanoe <bryan.leano@gmail.com>
2020-09-24 19:23:26 -0600

    add customer constructor

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -4,4 +4,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Customer {
+
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Customer(int id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
 }
