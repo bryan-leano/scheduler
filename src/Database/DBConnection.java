01419cd051b5f1899a7f098ade3de4d35dfd3b08
Bryan Leanoe <bryan.leano@gmail.com>
2020-10-01 20:57:28 -0600

    refactor dbconnection

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -18,14 +18,33 @@ public class DBConnection {
     private static final String username = "U06P8d";
     private static final String password = "53688828433";
 
-    public static void startConnection() throws ClassNotFoundException, SQLException {
-        Class.forName(MYSQLJDBCDriver);
-        conn = (Connection) DriverManager.getConnection(jdbcURL, username, password);
-        System.out.println("Connection successful!");
+    public static Connection startConnection()
+    {
+        try{
+            Class.forName(MYSQLJDBCDriver);
+            conn = (Connection)DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connection successful!");
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return conn;
     }
 
-    public static void closeConnection() throws SQLException {
-        conn.close();
-        System.out.println("Connection closed.");
+    public static void closeConnection() {
+        try{
+            conn.close();
+            System.out.println("Connection closed.");
+        }
+        catch(SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,16 @@
 package sample;
 
 import Database.DBConnection;
+import Database.DBQuery;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends Application {
 
@@ -19,9 +22,10 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DBConnection.startConnection();
+
         launch(args);
         DBConnection.closeConnection();
     }
