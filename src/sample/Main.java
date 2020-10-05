31f5a3e0f2b50f01376fb2cc5786182cb61c9569
Bryan Leanoe <bryan.leano@gmail.com>
2020-10-04 21:56:10 -0600

    added result set

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,9 +8,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main extends Application {
 
@@ -22,9 +27,27 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  SQLException {
 
-        DBConnection.startConnection();
+        Connection conn = DBConnection.startConnection();
+        DBQuery.setStatement(conn);
+        Statement statement = DBQuery.getStatement();
+
+        String selectStatement = "SELECT * FROM country";
+        statement.execute(selectStatement);
+        ResultSet rs = statement.getResultSet();
+
+        while(rs.next()) {
+            int countryId = rs.getInt("countryId");
+            String countryName = rs.getString("country");
+            LocalDate date = rs.getDate("createDate").toLocalDate();
+            LocalTime time = rs.getTime("createDate").toLocalTime();
+            String createdBy = rs.getString("createdBy");
+            LocalDateTime lastUpdate = rs.getTimestamp("lastUpdate").toLocalDateTime();
+
+            System.out.println(countryId + " | " + countryName + " | " + date + " | " + time
+                    + " | " + createdBy + " | " + lastUpdate);
+        }
 
         launch(args);
         DBConnection.closeConnection();
