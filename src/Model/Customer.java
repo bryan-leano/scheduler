c93c6d42d930cf24cd890e935cae9c5be0668265
Bryan Leanoe <bryan.leano@gmail.com>
2020-10-06 17:35:18 -0600

    update customer model

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -9,12 +9,18 @@ public class Customer {
     private String name;
     private String address;
     private String phone;
+    private String city;
+    private int zip;
+    private String country;
 
-    public Customer(int id, String name, String address, String phone) {
+    public Customer(int id, String name, String phone, String address, String city, int zip, String country) {
         this.id = id;
         this.name = name;
-        this.address = address;
         this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.country = country;
     }
 
     public int getId() {
@@ -25,27 +31,52 @@ public class Customer {
         return name;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
     public String getAddress() {
         return address;
     }
-     public String getPhone() {
-        return phone;
-     }
 
-     public void setId(int id) {
+    public String getCity() {
+        return city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setId(int id) {
         this.id = id;
-     }
+    }
 
-     public void setName(String name) {
+    public void setName(String name) {
         this.name = name;
-     }
+    }
 
-     public void setAddress(String address) {
-        this.address = address;
-     }
-
-     public void setPhone(String phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
-     }
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setCounty(String country) {
+        this.country = country;
+    }
 
 }
