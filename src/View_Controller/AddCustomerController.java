5d2f1e00bd7d7de7651e58c18cbbadc59cf6bae1
Bryan Leanoe <bryan.leano@gmail.com>
2020-10-08 22:31:01 -0600

    add countries and cities to add customer screen

--- src/View_Controller/AddCustomerController.java
+++ src/View_Controller/AddCustomerController.java
@@ -1,5 +1,6 @@
 package View_Controller;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -16,15 +17,55 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 
-public class AddCustomerController {
+public class AddCustomerController implements Initializable {
 
     Stage stage;
     Parent scene;
+
+    @FXML private TextField custIdTxt;
+    @FXML private TextField nameTxt;
+    @FXML private TextField phoneTxt;
+    @FXML private TextField addressTxt;
+    @FXML private ComboBox countryComboBox;
+    @FXML private ComboBox cityComboBox;
+    @FXML private TextField zipTxt;
+
+    private ObservableList<String> countries = FXCollections.observableArrayList("USA",
+            "UK", "Canada");
+
+    private ObservableList<String> usaCities = FXCollections.observableArrayList("New York",
+            "Chicago", "Portland");
+
+    private ObservableList<String> ukCities = FXCollections.observableArrayList("London",
+            "Manchester", "Liverpool");
+
+    private ObservableList<String> canadaCities = FXCollections.observableArrayList("Toronto",
+            "Vancouver", "Montreal");
 
     @FXML void onActionDisplayMain(ActionEvent event) throws IOException {
         stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
+    }
+
+    @Override public void initialize(URL location, ResourceBundle resources) {
+        countryComboBox.setItems(countries);
+        countryComboBox.getSelectionModel().selectFirst();
+
+        if(countryComboBox.getSelectionModel().getSelectedItem().equals("USA")) {
+            cityComboBox.setItems(usaCities);
+        }
+
+        countryComboBox.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
+            if(newValue.equals("USA")) {
+                cityComboBox.setItems(usaCities);
+            } else if (newValue.equals("UK")) {
+                cityComboBox.setItems(ukCities);
+            } else if (newValue.equals("Canada")) {
+                cityComboBox.setItems(canadaCities);
+            }
+        });
+
     }
 }
